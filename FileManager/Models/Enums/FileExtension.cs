ea7a6615ebc49b33d113236af8549f38cdbc61e9
jaugustyn <kub4a1@gmail.com>
2022-12-14 23:14:10 +0100

    Added models

---
+++ FileManager/Models/Enums/FileExtension.cs
@@ -0,0 +1,25 @@
+﻿namespace FileManager.Models.Enums;
+
+public enum FileExtension
+{
+    Pdf = 1,
+    Doc = 2,
+    Docx = 3,
+    Txt = 4,
+    Ppt = 5,
+    Pptx = 6,
+    Jpg = 7,
+    Jpeg = 8,
+    Png = 9,
+    Gif = 10,
+    Svg = 11,
+    Zip = 12,
+    Rar = 13,
+    Xlsx = 14,
+    Csv = 15,
+    Db = 16,
+    Sql = 17,
+    Xml = 18,
+    Log = 19,
+    
+}
---
+++ FileManager/Models/File.cs
@@ -0,0 +1,10 @@
+﻿using FileManager.Models.Enums;
+
+namespace FileManager.Models;
+
+public class File
+{
+    public int FileId { get; set; }
+    public string FileName { get; set; }
+    public FileExtension Extension { get; set; }
+}
